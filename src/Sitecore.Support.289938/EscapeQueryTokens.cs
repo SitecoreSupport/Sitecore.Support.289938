795923382057c5b168fb9e218a69e3e8401a8148
Calvin Williams <40271790+sc-CalvinWilliams@users.noreply.github.com>
2018-10-31 15:57:20 -0400

    init

---
+++ src/Sitecore.Support.289938/EscapeQueryTokens.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.XA.Foundation.TokenResolution.Pipelines.ResolveTokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.XA.Foundation.TokenResolution.Pipelines.ResolveTokens
+{
+  public class EscapeQueryTokens : ResolveTokensProcessor
+  {
+    public override void Process(ResolveTokensArgs args)
+    {
+      args.Query = EscapeTokens(args.Query);
+      if (args.Query == string.Empty)
+      {
+        args.Query = "/sitecore";
+      }
+    }
+
+    protected virtual string EscapeTokens(string query)
+    {
+      string[] array = query.Split('/');
+      for (int i = 0; i < array.Length; i++)
+      {
+        //Patch adds "|| array[i].Contains("-")" rest of class is default
+        if (array[i].Contains("$") || array[i].Contains("-"))
+        {
+          array[i] = "#" + array[i].Trim('#') + "#";
+        }
+      }
+      return array.DefaultIfEmpty("").Aggregate((string a, string b) => a + "/" + b);
+    }
+  }
+}
---
+++ src/Sitecore.Support.289938/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.289938")]
+[assembly: AssemblyProduct("Sitecore.Support.289938")]
+[assembly: ComVisible(false)]
